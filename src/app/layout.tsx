96e3ae61a5330338e2585e1eefd5f8ce0fe3633f
John Chinonso Edeh <netojaycee@gmail.com>
2026-04-22 17:35:15 +0100

    G

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/layout/ThemeProvider";
 import { Toaster } from "@/components/ui/sonner";
+import Script from "next/script";
+// import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
@@ -28,12 +30,30 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={bricolage.variable}
-      suppressHydrationWarning
-    >
+    <html lang="en" className={bricolage.variable} suppressHydrationWarning>
+      <head>
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PM5P42F4');
+          `}
+        </Script>
+      </head>
       <body className="min-h-screen bg-background text-foreground antialiased font-[family-name:var(--font-bricolage)]">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PM5P42F4"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+         {/* <GoogleTagManager gtmId="GTM-PM5P42F4" />
+         <GoogleAnalytics gaId="GTM-PM5P42F4" /> */}
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
