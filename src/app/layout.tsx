c16e083d115f6019658b3e218c293c6c40aa361b
John Chinonso Edeh <netojaycee@gmail.com>
2026-04-22 18:08:44 +0100

    G

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,26 +32,23 @@ export default function RootLayout({
   return (
     <html lang="en" className={bricolage.variable} suppressHydrationWarning>
       <head>
-        <Script id="gtm-script" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PM5P42F4');
-          `}
-        </Script>
+        
       </head>
       <body className="min-h-screen bg-background text-foreground antialiased font-[family-name:var(--font-bricolage)]">
         {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PM5P42F4"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
+       <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-6S8XZLN0M3"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6S8XZLN0M3');
+          `}
+        </Script>
          {/* <GoogleTagManager gtmId="GTM-PM5P42F4" />
          <GoogleAnalytics gaId="GTM-PM5P42F4" /> */}
         <ThemeProvider
